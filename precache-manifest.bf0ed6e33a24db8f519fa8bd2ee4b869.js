1bf63104b8b0514bac1f404a70501c1f44898afe
Dimassah <adaqqeq@gmail.com>
2020-02-23 21:21:52 +0500

    Updates

---
+++ precache-manifest.bf0ed6e33a24db8f519fa8bd2ee4b869.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe27a13aa9e5ad29078181afe79c7ceb",
+    "url": "/Dweather/index.html"
+  },
+  {
+    "revision": "c630b289f9c4d8be233b",
+    "url": "/Dweather/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b4cbc6181960b61d8ff0",
+    "url": "/Dweather/static/css/main.68ee78d8.chunk.css"
+  },
+  {
+    "revision": "c630b289f9c4d8be233b",
+    "url": "/Dweather/static/js/2.303c316c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Dweather/static/js/2.303c316c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cbc6181960b61d8ff0",
+    "url": "/Dweather/static/js/main.4e3a2c04.chunk.js"
+  },
+  {
+    "revision": "7a1d368fae20dce80cf4",
+    "url": "/Dweather/static/js/runtime-main.44688a09.js"
+  },
+  {
+    "revision": "2dd7abf2f106bf24156b9ea4e349ea8f",
+    "url": "/Dweather/static/media/bg.2dd7abf2.jpg"
+  }
+]);
